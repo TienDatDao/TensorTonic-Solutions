0cbb60465d5d656317a07bc4d71d0d9568ee485e
Dao Tien Dat <94846751+TienDatDao@users.noreply.github.com>
2026-03-01 13:11:46 +0700

    Update solution for vector-norm-3d

--- vector-norm-3d/vector-norm-3d.py
+++ vector-norm-3d/vector-norm-3d.py
@@ -2,10 +2,8 @@ import numpy as np
 
 def vector_norm_3d(v):
     
-    v = np.asarray(v)
-    if(v.ndim == 1):
-        v = v[np.newaxis, : ]
-    ans = np.sqrt(np.sum(v**2, axis = 1))
+    v = np.atleast_2d(v)
+    ans = np.linalg.norm(v, ord=2, axis=1)
     if(len(ans) == 1):
         return ans[0]
     return ans
